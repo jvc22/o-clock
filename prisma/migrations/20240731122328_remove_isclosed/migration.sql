1771ca7160b76894c96df8ca7bd8ee13381f6eb0
joaovcostas <jvc22102003@gmail.com>
2024-07-31 10:17:24 -0300

    db: remove isClosed from notes table

---
+++ prisma/migrations/20240731122328_remove_isclosed/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isClosed` on the `notes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "notes" DROP COLUMN "isClosed";
