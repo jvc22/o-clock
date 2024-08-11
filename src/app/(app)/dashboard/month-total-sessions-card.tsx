acfc13c83a46331db5629d69e7b7f65c9388c976
joaovcostas <jvc22102003@gmail.com>
2024-08-11 20:51:49 -0300

    feat: implement total month sessions component and api route

--- src/app/(app)/dashboard/month-total-sessions-card.tsx
+++ src/app/(app)/dashboard/month-total-sessions-card.tsx
@@ -1,8 +1,21 @@
-import { CalendarRange } from 'lucide-react'
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+import { CalendarRange, Loader2 } from 'lucide-react'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { getMonthTotalSessions } from '@/http/get-month-total-sessions'
+
+import { CardSkeleton } from './card-skeleton'
 
 export function MonthTotalSessionsCard() {
+  const today = new Date().toDateString()
+
+  const { data: monthTotalSessions, isFetching } = useQuery({
+    queryKey: ['metrics', 'total-sessions'],
+    queryFn: () => getMonthTotalSessions({ today }),
+  })
+
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -10,15 +23,39 @@ export function MonthTotalSessionsCard() {
           Total sessions (month)
         </CardTitle>
 
-        <CalendarRange className="size-4 text-muted-foreground" />
+        {isFetching ? (
+          <Loader2 className="size-4 animate-spin text-muted-foreground" />
+        ) : (
+          <CalendarRange className="size-4 text-muted-foreground" />
+        )}
       </CardHeader>
 
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold">184</span>
+        {monthTotalSessions ? (
+          <>
+            <span className="text-2xl font-bold">
+              {monthTotalSessions.amount}
+            </span>
 
-        <p className="text-xs text-muted-foreground">
-          <span className="text-emerald-500">+2%</span> compared to last month
-        </p>
+            <p className="text-xs text-muted-foreground">
+              <span
+                className={
+                  monthTotalSessions.diffFromLastMonth >= 0
+                    ? 'text-emerald-500'
+                    : 'text-red-500'
+                }
+              >
+                {monthTotalSessions.diffFromLastMonth >= 0
+                  ? `+${monthTotalSessions.diffFromLastMonth}`
+                  : `${monthTotalSessions.diffFromLastMonth}`}
+                %
+              </span>{' '}
+              compared to last month
+            </p>
+          </>
+        ) : (
+          <CardSkeleton />
+        )}
       </CardContent>
     </Card>
   )
