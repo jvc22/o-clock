0f49711151cc6acb99fcae3b150b517d36e3afbe
joaovcostas <jvc22102003@gmail.com>
2024-09-20 12:51:37 -0300

    fix: reorganize icons declaration order

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,30 +24,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
-        <meta name="theme-color" content="#020817" />
-        <meta name="msapplication-TileImage" content="/icon-144x144.png" />
-
-        <link rel="apple-touch-icon" sizes="57x57" href="/icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icon-180x180.png" />
         <link
           rel="apple-touch-icon"
           sizes="1024x1024"
           href="/icon-1024x1024.png"
         />
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon-180x180.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/icon-57x57.png" />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="1024x1024"
+          href="/icon-1024x1024.png"
+        />
         <link
           rel="icon"
           type="image/png"
@@ -72,6 +70,15 @@ export default function RootLayout({
           sizes="16x16"
           href="/icon-16x16.png"
         />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="theme-color" content="#020817" />
+        <meta name="msapplication-TileImage" content="/icon-144x144.png" />
 
         <link rel="manifest" href="/manifest.json" />
       </head>
